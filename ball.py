2d9568285fe93fd967a9b31eb252227b915a6862
Dong-ho-nim <shindh758@gmail.com>
2025-11-04 15:40:45 +0900

    수업

---
+++ ball.py
@@ -0,0 +1,36 @@
+from pico2d import *
+import game_world
+import game_framework
+
+PIXEL_PER_METER = (1.0 / 0.03)  # 1pixel = 3cm, 1m = 33.33 pixel
+GRAVITY = 9.8  # 중력 가속도 (m/s²)
+
+class Ball:
+    image = None
+
+    def __init__(self, x = 400, y = 300, velocity = 1):
+        if Ball.image == None:
+            Ball.image = load_image('ball21x21.png')
+        self.x, self.y, self.velocity = x, y, velocity
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+        draw_rectangle(*self.get_bb())
+
+
+    def update(self):
+        self.x += self.velocity * 100 * game_framework.frame_time
+
+        if self.x < 25 or self.x > 1600 - 25:
+            game_world.remove_object(self)
+
+    def get_bb(self):
+        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
+
+    def handle_collision(self, group, other):
+        if group == 'boy:ball':
+            game_world.remove_object(self)
+
+
+
+
