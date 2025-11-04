d1c8b586fc43a935a6abfd45d96a0d7fc03ea739
Dong-ho-nim <shindh758@gmail.com>
2025-11-04 16:16:28 +0900

    코드 수정

--- play_mode.py
+++ play_mode.py
@@ -26,7 +26,9 @@ def init():
     global grass
     global boy
     running = True
-    global zombie
+    global zombies
+    global balls
+
 
     grass = Grass()
     game_world.add_object(grass, 0)
@@ -34,18 +36,21 @@ def init():
     boy = Boy()
     game_world.add_object(boy, 1)
 
-    global balls
     balls = [Ball(random.randint(100, 1600 - 100), 60, 0) for _ in range(30)]
     game_world.add_objects(balls, 1)
+
+    zombies = [Zombie() for _ in range(4)]
+    game_world.add_objects(zombies, 1)
+
+
     for ball in balls:
         game_world.add_collision_pair('boy:ball', boy, ball)
         game_world.add_collision_pair('zombie:ball', boy, ball)
-    for zombie in zombies:
-        game_world.add_collision_pair('zombie:ball', boy, zombie)
 
-    global zombies
-    zombies = [Zombie() for _ in range(4)]
-    game_world.add_objects(zombies, 1)
+    for zombie in zombies:
+        game_world.add_collision_pair('zombie:ball', zombie, boy)
+
+
 
 def finish():
     game_world.clear()
